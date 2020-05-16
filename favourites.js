fdd588a1cc24cab99975d6fa2869d7019eda1d63
s1hagog <moshakalex@gmail.com>
2020-05-16 15:28:06 -0700

    code refactor

---
+++ favourites.js
@@ -0,0 +1,30 @@
+const gallery = document.querySelector('.gallery');
+let imagesArray = [];
+
+//Checking that our JSON string is correct
+try {
+    imagesArray = JSON.parse(localStorage.getItem('favitem'));
+} catch (error) {
+    console.log(error);
+}
+
+//Check that it is not empty even if correct;
+if (imagesArray && imagesArray.length != 0) {
+    imagesArray.forEach((img) => {
+        const galleryFavImg = document.createElement('div');
+        galleryFavImg.classList.add('gallery-img');
+        galleryFavImg.innerHTML = `
+                    <div class="gallery-info">
+                        <p>Sample Info</p>
+                    </div>
+                    <img src="${img}" />
+                `;
+        gallery.appendChild(galleryFavImg);
+    });
+} else {
+    //Display error message
+    const errorText = document.createElement('h1');
+    errorText.classList.add('error-message');
+    errorText.innerText = 'Sorry, no favourite images to show';
+    document.querySelector('main').appendChild(errorText);
+}
